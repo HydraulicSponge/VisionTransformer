fdb5c7be8c1eb6387d908a3ca40141b249ae406e
Sarim <108764082+HydraulicSponge@users.noreply.github.com>
2024-03-04 22:04:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ import torch.nn.functional as F
 from torchvision.transforms import Resize, ToTensor
 
 
-path_train = r"C:\Users\Sarim\OneDrive\Desktop\ViTs for DBRP\data\training_data"
-path_test = r"C:\Users\Sari\OneDrive\Desktop\ViTs for DBRP\data\testing_data"
+path_train = r"C:\Users\Sarim&Sahar\OneDrive\Desktop\ViTs for DBRP\data\training_data"
+path_test = r"C:\Users\Sarim&Sahar\OneDrive\Desktop\ViTs for DBRP\data\testing_data"
 
 
 class Compose(object):
@@ -31,7 +31,6 @@ class Compose(object):
             image = t(image)
         return image, target
 
-
 classes = ['healthy', 'mild npdr', 'moderate npdr', 'severe npdr', 'pdr']
 
 
@@ -40,67 +39,89 @@ for i in classes:
     num_images = len([file for file in os.listdir(class_path) if file.endswith(('jpg', 'jpeg', 'png'))])
     print(f"class: {i}, num of datapoints: {num_images}")
 
+
 from torchvision import transforms
 
+"""
 class CustomDataset(Dataset):
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
         self.classes = os.listdir(root_dir)
-        self.images = []
         self.class_to_idx = {cls: i for i, cls in enumerate(self.classes)}
-
+        
+        # Shuffle image paths
+        self.image_paths = [] 
         for cls in self.classes:
             cls_path = os.path.join(root_dir, cls)
-            if os.path.isdir(cls_path):
-                cls_images = [os.path.join(cls_path, img) for img in os.listdir(cls_path)]
-                self.images.extend([(img, self.class_to_idx[cls]) for img in cls_images])
+            self.image_paths.extend(glob.glob(os.path.join(cls_path, '*.*')))
+        random.shuffle(self.image_paths) 
 
     def __len__(self):
-        return len(self.images)
+        return len(self.image_paths)
 
     def __getitem__(self, idx):
-        img_path, label = self.images[idx]
+        img_path = self.image_paths[idx]
         image = Image.open(img_path)
-        
+
         if self.transform:
             image = self.transform(image)
-            
-        return image, label
+    
+    # Get class name from path
+        class_name = img_path.split('/')[-2]  
 
+    # Get class index 
+        label = self.class_to_idx[class_name]
+
+        return image, label
+"""
+from torchvision.datasets import ImageFolder
 data_transform = transforms.Compose([
     transforms.Resize((144, 144)),
-  #[Augmentation if needed]
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(20),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
     transforms.ToTensor()
-])
+    ])
 
 
-dataset = CustomDataset(root_dir=path_train, transform=data_transform)
+dataset = ImageFolder(root=path_train, transform=data_transform)
 
-class PatchEmbedding(nn.Module):
-    def __init__(self, in_channels=3, patch_size=8, emb_size=256):
-        self.patch_size = patch_size
-        super().__init__()
-        self.projection = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size),
-            nn.Linear((patch_size **  2) * in_channels, emb_size)
-        )
+import matplotlib.pyplot as plt
+import random
 
-        #initialize linear layer weights randomly
-        nn.init.xavier_uniform_(self.projection[1].weight)
+num_rows = 5
+num_cols = 5
 
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.projection(x)
-        return x
-      
-label_mapping = {
-    0: "healthy",
-    1: "mild npdr",
-    2: "moderate npdr",
-    3: "severe npdr",
-    4: "pdr"
-}
+
+fig, axs = plt.subplots(num_rows, num_cols, figsize=(10, 10))
+to_pil = transforms.ToPILImage() 
+
+for i in range(num_rows):
+    for j in range(num_cols):
+
+        image_index = random.randrange(len(dataset))
+
+        axs[i, j].imshow(dataset[image_index][0].permute((1, 2, 0)))
+
+
+        axs[i, j].set_title(dataset.classes[dataset[image_index][1]], color="black")
+        image, label = dataset[image_index]
+        pil_image = to_pil(image)
+
+        axs[i, j].axis(False)
+        axs[i, j].imshow(pil_image)
+
+
+fig.suptitle(f"Random {num_rows * num_cols} images from the training dataset", fontsize=16, color="blue")
+
+fig.set_facecolor(color='white')
+
+plt.show()
+
+"""
 def show_images(dataset, num_samples=20, cols=4):
+    # Get a random subset of indices
     random_dataset = random.sample(list(range(len(dataset))), num_samples)
     plt.figure(figsize=(15, 15))
     for i, idx in enumerate(random_dataset):
@@ -113,7 +134,33 @@ def show_images(dataset, num_samples=20, cols=4):
 
     plt.show()
 
-show_images(dataset)
+show_images(dataset)"""
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, in_channels=3, patch_size=8, emb_size=256):
+        self.patch_size = patch_size
+        super().__init__()
+        self.projection = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size),
+            nn.Linear((patch_size **  2) * in_channels, emb_size)
+        )
+
+        # Initialize the linear layer weights randomly
+        nn.init.xavier_uniform_(self.projection[1].weight)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.projection(x)
+        return x
+
+
+label_mapping = {
+    0: "healthy",
+    1: "mild npdr",
+    2: "moderate npdr",
+    3: "severe npdr",
+    4: "pdr"
+}
+
 
     
 class Attention(nn.Module):
@@ -136,15 +183,15 @@ class Attention(nn.Module):
         return attn_output
 
 Attention(dim=256, n_heads=4, dropout=0.1)(torch.ones((1, 5, 256))).shape
-  
+
 
 
 sample_datapoint = torch.unsqueeze(dataset[0][0], 0)
-print("Initial shape: ", sample_datapoint.shape) # 1 = batch dimention, 3 = color channels, dimensions = 144 by 144
+print("Initial shape: ", sample_datapoint.shape) 
 embedding = PatchEmbedding()(sample_datapoint)
-print("Patches shape: ", embedding.shape) # After applyign the patch embeedding, there are 324 patches, and each of the patches of a dimension of 128*128
-                                        # We get the number 324, because number of patches = (image height/patch height)(image width/patch width)
-                                        #AND our intial image shape was set to 144*144. 144 divided by our patch size of 8 equals to 18. 18 squared = 324
+print("Patches shape: ", embedding.shape)
+
+
 
 
 class PreNorm(nn.Module):
@@ -158,6 +205,7 @@ class PreNorm(nn.Module):
 norm = PreNorm(256, Attention(dim=256, n_heads=4, dropout=0.1))
 norm(torch.ones((1, 5, 256))).shape
 
+#==
 
 class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout=0.1):
@@ -203,15 +251,14 @@ class PositionalEncoding(nn.Module):
 
     def forward(self, x):
         return x + self.pe[:, :x.size(1)].detach()
-      
 class ModelArgs:
     dim: int = 256         
-    hidden_dim: int = 512   
-    n_heads: int = 8       
+    hidden_dim: int = 512  
+    n_heads: int = 8        
     n_layers: int = 6       
     patch_size: int = 8     
-    n_channels: int = 3    
-    n_patches: int = 1024    
+    n_channels: int = 3     
+    n_patches: int = 1024  
     n_classes: int = 5   
     dropout: float = 0.2
     in_channels: int=3   
@@ -242,90 +289,58 @@ class ViT(nn.Module):
         assert h == w, 'Input tensor shape must be square'
 
         x = self.patch_embed(x)
-        x = x.permute(1, 0, 2) 
+        x = x.permute(1, 0, 2)  
         cls_token = self.cls_token.expand(-1, b, -1)
         x = torch.cat((cls_token, x), dim=0)
         x = self.pos_encoding(x)
 
-     
         x = self.transformer(x)
+
         cls_token_final = x[0]
+
+
         x = self.mlp_head(cls_token_final)
+
         return x
+
 
 device = "cpu"
 model = ViT(args=ModelArgs()).to(device)
 model(torch.ones((1, 3, 144, 144)))
 
-train_split = int(0.8 * len(dataset))
-train, test = random_split(dataset, [train_split, len(dataset) - train_split])
+test_dataset = ImageFolder(root=path_test, transform=data_transform)
 
-train_dataloader = DataLoader(train, batch_size=32, shuffle=True)
-test_dataloader = DataLoader(test, batch_size=32, shuffle=False)
+train_dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 correct_predictions = 0
 total_samples = 0
 
-optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=1e-4)
+optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=1e-2)
 
 criterion = nn.CrossEntropyLoss()
 from torch.utils.data import SubsetRandomSampler
 
+
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
-  
 train_subset_sampler = SubsetRandomSampler(range(100))
 test_subset_sampler = SubsetRandomSampler(range(100))
-subset_train_dataloader = DataLoader(train.dataset, batch_size=32, sampler=train_subset_sampler)
-subset_test_dataloader = DataLoader(test.dataset, batch_size=32, sampler=test_subset_sampler)
 
+#subset_train_dataloader = DataLoader(dataset, batch_size=32, sampler=train_subset_sampler)
+#subset_test_dataloader = DataLoader(test_dataset, batch_size=32, sampler=test_subset_sampler)
 lr = []
 num_params = count_parameters(model)
 print(f"Number of parameters in the model: {num_params}")
-#========================================================================================================================
-class EarlyStopping:
-    def __init__(self, patience=4, verbose=False, delta=0, path=r'C:\Users\Sarim&Sahar\OneDrive\Desktop\vv\save_data.pth', trace_func=print):
-        self.patience = patience
-        self.verbose = verbose
-        self.counter = 0
-        self.best_score = None
-        self.early_stop = False
-        self.val_loss_min = np.Inf
-        self.delta = delta
-        self.path = path
-        self.trace_func = trace_func
-    def __call__(self, val_loss, model):
-
-        score = -val_loss
-
-        if self.best_score is None:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model)
-        elif score < self.best_score + self.delta:
-            self.counter += 1
-            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
-            if self.counter >= self.patience:
-                self.early_stop = True
-        else:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model)
-            self.counter = 0
-
-    def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
-        if self.verbose:
-            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), self.path)
-        self.val_loss_min = val_loss
+from pytorchtools import EarlyStopping
 scheduler =torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=3, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0.00000001, eps=1e-06, verbose=1)
 earlystopping = EarlyStopping(patience=3, verbose=True)
-#========================================================================================================================
-num_epochs = 20
-
+num_epochs = 1
 for epoch in range(num_epochs):  
     train_losses = []
     train_correct_predictions = 0
     train_total_samples = 0
     
+    # Training phase
     model.train()
     for step, (inputs, labels) in enumerate(train_dataloader):
         optimizer.zero_grad()
@@ -336,17 +351,18 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         train_losses.append(loss.item())
-      
+        
+
         _, predicted = torch.max(outputs, 1)
         train_correct_predictions += torch.sum(predicted == labels).item()
         train_total_samples += labels.size(0)
 
     train_accuracy = train_correct_predictions / train_total_samples
-    
+
     model.eval()
-    val_losses = []
-    val_correct_predictions = 0
-    val_total_samples = 0
+    test_losses = []
+    test_correct_predictions = 0
+    test_total_samples = 0
     
     with torch.no_grad():
         for step, (inputs, labels) in enumerate(test_dataloader):
@@ -355,28 +371,27 @@ for epoch in range(num_epochs):
 
             outputs = model(inputs)
             loss = criterion(outputs, labels)
-            val_losses.append(loss.item())
+            test_losses.append(loss.item())
             
+            #_, predicted = torch.max(outputs, 1)
+            test_correct_predictions += torch.sum(predicted == labels).item()
+            test_total_samples += labels.size(0)
 
-            _, predicted = torch.max(outputs, 1)
-            val_correct_predictions += torch.sum(predicted == labels).item()
-            val_total_samples += labels.size(0)
-
-    val_accuracy = val_correct_predictions / val_total_samples
+    test_accuracy = test_correct_predictions / test_total_samples
     
     print(f">>> Epoch {epoch+1} train loss: {np.mean(train_losses)} train accuracy: {train_accuracy}")
-    print(f">>> Epoch {epoch+1} test loss: {np.mean(val_losses)} test accuracy: {val_accuracy}")
+    print(f">>> Epoch {epoch+1} test loss: {np.mean(test_losses)} test accuracy: {test_accuracy}")
 
 
-    val_loss_avg = np.mean(val_losses)
-    earlystopping(val_loss_avg, model)
-    scheduler.step(np.mean(val_losses))
+    test_loss_avg = np.mean(test_losses)
+    earlystopping(test_loss_avg, model)
+    scheduler.step(np.mean(test_losses))
     if earlystopping.early_stop:
         print("Early stopping")
         break
 
-    # Update the learning rate based on the validation loss
-    scheduler.step(np.mean(val_losses))
+
+    scheduler.step(np.mean(test_losses))
     print('epoch={}, learning rate={:.4f}'.format(epoch + 1, optimizer.state_dict()['param_groups'][0]['lr']))
     lr.append(optimizer.state_dict()['param_groups'][0]['lr'])
 
