d6c05668ac3e0d2a4936a5eb3286e411f4ca27c6
Sarim <108764082+HydraulicSponge@users.noreply.github.com>
2024-03-18 18:22:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,13 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torch.utils.data import random_split
 import torchvision.datasets as datasets
-import torchvision.transforms as transforms 
+import torchvision.transforms as transforms
 from torchvision.transforms.functional import to_pil_image
+import torchvision.models as models
+import torch.onnx
+from torch.utils.data import Subset
+from sklearn.model_selection import train_test_split
+
 torch.cuda.empty_cache()
 path_train = '/kaggle/input/diabetic-retinopathy-resized-arranged'
 classes = ['0', '1', '2', '3', '4']
@@ -32,19 +37,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 output_dir = '/kaggle/working/'
 
-# Create the output directory if it doesn't exist
+
 if not os.path.exists(output_dir):
     os.makedirs(output_dir, exist_ok=True)
    
-# set seed
 def set_random_seed(seed: int) -> None:
-    """
-    Sets the seeds at a certain value.
-    :param seed: the value to be set
-    Also, need to add "worker_init_fn=np.random.seed(seed)" in dataloader
-    # https://discuss.pytorch.org/t/determinism-in-pytorch-across-multiple-files/156269
-    # https://stackoverflow.com/questions/65685060/unique-seed-acrossing-multiple-imported-files-with-random-module-python
-    """
+
     print(f"Setting seeds: {seed} ...... ")
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -57,14 +55,11 @@ def set_random_seed(seed: int) -> None:
     torch.backends.cudnn.deterministic=  True
    
 def worker_init_fn(worker_id):    
-    '''
-    this function is for dataloader's worker_init_fn
-    '''                                                    
+                                                 
     np.random.seed(np.random.get_state()[1][0] + worker_id)
 
 set_random_seed(123)
 
-# Define a custom dataset class for loading and preprocessing data
 def make_weights_for_balanced_classes(labels):
     count = torch.bincount(torch.tensor(labels)).to(device)
     print('Count:', count.cpu().detach().numpy())
@@ -82,58 +77,14 @@ path_val = path_train
 path_test = path_train
 
 
-"""
-train_dataloader = DataLoader(path_train, batch_size=128, shuffle=True)
-test_dataloader = DataLoader(path_test, batch_size=128, shuffle=False)
-"""
-# Create data loaders
-batch_size = 64
+
+batch_size = 32
 IMAGE_SIZE = 224
-IMAGENET_MEAN = [0.5,0.5,0.5]         # Mean of ImageNet dataset (used for normalization)
-IMAGENET_STD = [0.5,0.5,0.5]          # Std of ImageNet dataset (used for normalization)
+IMAGENET_MEAN = [0.485, 0.456, 0.406]         # Mean of ImageNet dataset (used for normalization)
+IMAGENET_STD = [0.229, 0.224, 0.225]         # Std of ImageNet dataset (used for normalization)
 
 
-# T.Compose([
-#     T.RandomResizedCrop(size=IMAGE_SIZE, scale=(0.8, 1.2), ratio=(1, 1)),
-#     T.RandomApply([T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05)], p=0.5),
-#     T.RandomApply([T.RandomVerticalFlip()], p=0.5),
-#     T.RandomRotation(degrees=20),
-#     T.RandomResizedCrop(size=IMAGE_SIZE, scale=(0.8, 1.2), ratio=(1, 1)),
-# ])
-from torch.utils.data import Subset
-from sklearn.model_selection import train_test_split
-#_____________________
-"""
-transform = T.Compose([
-    T.Resize((224,224)),
-    T.ToTensor(),
-])
 
-dataset = datasets.ImageFolder(path_train, transform=transform)
-
-def get_mean_std(loader):
-    # Compute the mean and standard deviation of all pixels in the dataset
-    num_pixels = 0
-    mean = 0.0
-    std = 0.0
-    for images, _ in loader:
-        batch_size, num_channels, height, width = images.shape
-        num_pixels += batch_size * height * width
-        mean += images.mean(axis=(0, 2, 3)).sum()
-        std += images.std(axis=(0, 2, 3)).sum()
-
-    mean /= num_pixels
-    std /= num_pixels
-
-    return mean, std
-
-
-loader = DataLoader(dataset, batch_size=64, shuffle=True)
-
-mean, std = get_mean_std(loader)
-print(mean, std)
-"""
-#_____________________________
 train_transform = T.Compose([
     T.Resize((IMAGE_SIZE, IMAGE_SIZE)),
     T.RandomAffine(degrees=20, translate=(0, 0), scale=(0.8, 1.2), shear=0),
@@ -141,19 +92,19 @@ train_transform = T.Compose([
     T.RandomApply([T.RandomVerticalFlip()], p=0.5),
     T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),
     T.ToTensor(),
-    #T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
+    T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
 ])
 
 test_transform = T.Compose([
     T.Resize((IMAGE_SIZE, IMAGE_SIZE)),
     T.ToTensor(),
-    #T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
+    T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
 ])
 
 dataset = ImageFolder(path_train, transform=train_transform)
 dataset_test = ImageFolder(path_train, transform=test_transform)
 targets = dataset.targets
-                
+               
 train_idx, valid_idx= train_test_split(
 np.arange(len(targets)),
 test_size=0.2,
@@ -168,7 +119,6 @@ import matplotlib.pyplot as plt
 import random
 
 
-
 label_mapping = {
     0: "healthy",
     1: "mild npdr",
@@ -177,7 +127,7 @@ label_mapping = {
     4: "pdr"
 }
 def show_images(dataset, num_samples=20, cols=4):
-    # Get a random subset of indices
+
     random_dataset = random.sample(list(range(len(dataset))), num_samples)
     plt.figure(figsize=(15, 15))
     for i, idx in enumerate(random_dataset):
@@ -194,7 +144,7 @@ def show_images(dataset, num_samples=20, cols=4):
 show_images(dataset)
 
 
-# For unbalanced dataset we create a weighted sampler                      
+                   
 weights = make_weights_for_balanced_classes(train_dataset.targets)
 weighted_sampler = sampler.WeightedRandomSampler(weights, len(weights))
 
@@ -205,33 +155,33 @@ val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, pin_m
 
 
 
-# Training loop
 
 best_test_acc = 0
 best_epoch = 0
 num_classes = 5
-train_losses = []  # To store training losses
-val_losses = []    # To store validation losses
-train_accuracies = []  # To store training accuracies
-val_accuracies = []    # To store validation accuracies
+train_losses = []  
+val_losses = []  
+train_accuracies = []
+val_accuracies = []  
 logs = ''
 
 
-# Define the model architecture (ResNet-50 as an example)
-print("Model: ViT")
-model = models.vit_b_16(weights='IMAGENET1K_V1')
 
-#hidden_layer_size = 512
-#num_ftrs = model.fc.in_features
+print("Model: ViT")
+model = models.vit_b_16(weights='IMAGENET1K_V1').to(device)#weights='IMAGENET1K_V1'
+#model.load_state_dict(torch.load('/kaggle/input/yoloweights/yolov5s-seg.pt'))
+
+#model = torch.load('/kaggle/input/model-architecture/model_architecture (3).pth')##########################################load
+#model.load_state_dict(torch.load('/kaggle/input/model-weights/model_weights (3).pth'))############################load
+
+
+
 model.heads = nn.Sequential(
-   # nn.Linear(num_ftrs, hidden_layer_size),
-    #nn.ReLU(),
-    # nn.BatchNorm1d(hidden_layer_size),
-    #nn.Dropout(0.2),
     nn.Linear(in_features=768, out_features=5, bias=True)
-    #nn.Linear(hidden_layer_size, num_classes)
+
+
 )
-#model.load_state_dict(torch.load(r'C:\Users\Sarim&Sahar\OneDrive\Desktop\Science Fair ViT\save_data.pth'), strict=False)
+
 model = model.to(device)
 
 class Temperature(nn.Module):
@@ -242,23 +192,29 @@ class Temperature(nn.Module):
     def forward(self, x):
         return x / torch.exp(self.T)
 
-# Define loss function and optimizer
+
 count = torch.bincount(torch.tensor(train_dataset.targets)).to(device)
-class_weight = len(train_dataset.targets) / count # (count*num_classes)
+class_weight = len(train_dataset.targets) / count
 
 print('Loss class weight:', class_weight)
-class_weight = None
-criterion = nn.CrossEntropyLoss(weight=class_weight).to(device)
+criterion = torch.nn.CrossEntropyLoss(weight=class_weight).to(device)
 params = list(model.parameters())
-optimizer = optim.SGD(params, lr=1e-4, weight_decay=1e-5)#, eps=1e-8)
+optimizer = optim.SGD(params, lr=0.0001, weight_decay=1e-4)#, eps=1e-8)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=4, mode='min')
 
 accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=num_classes, average='weighted').to(device)
 confmat = torchmetrics.ConfusionMatrix(task="multiclass", num_classes=num_classes, normalize='true').to(device)
 class_accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=num_classes, average=None).to(device)
-num_epochs = 15
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+num_params = count_parameters(model)
+print(f"Number of parameters in the model: {num_params}")
 
 #TRAINING
+num_epochs = 12
 for epoch in range(num_epochs):
     t = tqdm(enumerate(train_loader, 0), total=len(train_loader),
                 smoothing=0.9, position=0, leave=True,
@@ -309,64 +265,41 @@ for epoch in range(num_epochs):
            
    
     val_class_accuracy = class_accuracy.compute()  
-    # class_weight = torch.tensor(
-    #     [1.0 / ((acc.item() + 1e-5) * cls_count.item()) for acc, cls_count in zip(val_class_accuracy, torch.bincount(torch.tensor(test_dataset.labels)))]
-    # ).to(device)  
-   
-    # Exponential decay for updating class weights based on accuracy
-    # accuracy_weights = 0.1 * (1.0 / (val_class_accuracy+1e-5)) + (1 - 0.1) * torch.ones(num_classes).to(device)
-    # # Exponential decay for updating class weights based on frequency
-    # frequency_weights = 0.1 * (1.0 / count) + (1 - 0.1) * torch.ones(num_classes).to(device)
-    # # Combine accuracy and frequency weights
-    # class_weight = 0.1 * (accuracy_weights * frequency_weights).to(device) + (1-0.1)*class_weight
-    # class_weight = class_weight.to(device)
-    # class_weight=None
-   
-    # print(class_weight)
-    # logs+=f'Class loss weight: {list(class_weight.cpu().detach().numpy())}\n'    
-
+ 
     val_loss = val_loss / len(val_loader)
     val_losses.append(val_loss)
     val_accuracy = accuracy.compute()
     val_accuracies.append(float(val_accuracy))
 
-    test_loss = val_loss # test_loss / len(test_loader)
+    test_loss = val_loss
    
-    # Calculate metrics for test data
+
     test_accuracy = val_accuracy
-   
-    # scheduler
+
     scheduler.step(val_loss)
     lr_log = f"LR: {optimizer.param_groups[0]['lr']}" # scheduler._last_lr
     print(lr_log)
     logs+=lr_log+'\n'
-   
-    # Print and log epoch results
+ 
     train_results = f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Training Accuracy: {train_accuracy}, Validation Loss: {val_loss}, Validation Accuracy: {val_accuracy}"
     print(train_results)
-    test_results = f"Test Accuracy: {test_accuracy}, Test Loss: {test_loss}"
-    print(test_results)
-    logs+=train_results+'\n'+test_results+'\n'
-    # print(confmat.compute())
-   
-    # Save the model checkpoint
-    torch.save(model.state_dict(), os.path.join(output_dir, f'last.pth'))
+    logs+=train_results+'\n'
+    torch.save(model.state_dict(), os.path.join(output_dir, f'last.pth')) #Save model checkpoint
    
     if (epoch+1)%5==0:
         torch.save(model.state_dict(), os.path.join(output_dir, f'epoch{epoch+1}.pth'))
 
-        # fig, ax = confmat.plot()
+     
         fig, ax = plt.subplots()
         confmat_vals = np.around(confmat.compute().cpu().detach().numpy(), 3)
         im = ax.imshow(confmat_vals)
 
-        # Show all ticks and label them with the respective list entries
         ax.set_xticks(np.arange(num_classes))
         ax.set_yticks(np.arange(num_classes))
         ax.set_xlabel('Predicted class')
         ax.set_ylabel('True class')
 
-        # Loop over data dimensions and create text annotations.
+     
         for i in range(num_classes):
             for j in range(num_classes):
                 text = ax.text(j, i, confmat_vals[i, j],ha="center", va="center", color="black", fontsize=12)
@@ -374,10 +307,7 @@ for epoch in range(num_epochs):
         ax.set_title(f"Confusion Matrix on Test for epoch {epoch+1}")
         fig.savefig(os.path.join(output_dir, f"conf_mat_epoch{epoch+1}.png"))
         plt.close()
-       
-        # class_weight = 0.3 * (1.0 / (val_class_accuracy+1e-5)) + (1-0.3)*class_weight
-        # print(class_weight)
-        # logs+=f'Class loss weight: {list(class_weight.cpu().detach().numpy())}\n'  
+     
    
     if best_test_acc <= test_accuracy and epoch!=0:
         best_epoch = epoch+1
@@ -386,8 +316,8 @@ for epoch in range(num_epochs):
         logs+=log+"\n"
         best_test_acc = test_accuracy
         torch.save(model.state_dict(), os.path.join(output_dir, f'best.pth'))
-
-        # fig, ax = confmat.plot()
+       
+   
         fig, ax = plt.subplots()
         confmat_vals = np.around(confmat.compute().cpu().detach().numpy(), 3)
         im = ax.imshow(confmat_vals)
@@ -407,16 +337,16 @@ for epoch in range(num_epochs):
         fig.savefig(os.path.join(output_dir, "conf_mat_best.png"))
         plt.close()
    
-    # resetting all metrics
+ 
     accuracy.reset(); class_accuracy.reset(); confmat.reset()
    
 
-# Save the printed outputs to a log.txt file
+#Save the printed outputs to a log.txt file
 with open(os.path.join(output_dir, 'log.txt'), 'w') as log_file:
     log_file.write(logs)
-    log_file.write(f'Best test accuracy: {best_test_acc} in epoch {best_epoch}')
+    log_file.write(f'Best val accuracy: {best_test_acc} in epoch {best_epoch}')
+os.makedirs(output_dir, exist_ok=True)
 
-# Save the loss and accuracy graphs
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
 plt.plot(range(1, num_epochs+1), train_losses, label='Train Loss')
@@ -438,13 +368,41 @@ plt.savefig(os.path.join(output_dir, 'loss_accuracy_graph.png'))
 plt.close()
 
 
-print("DR classifier model completed")
-print("Model saved location :", output_dir)
 
+torch.save(model, os.path.join('model_architecture.pth'))#############################################save
+torch.save(model.state_dict(), os.path.join(output_dir, f'model_weights.pth'))########################save
+
+
+#TESTING
 model.eval()
-inputs, labels = next(iter(test_loader))
-inputs, labels = inputs.to(device), labels.to(device)
-outputs = model(inputs)
-print(outputs)
-print("Predicted classes", outputs.argmax(-1))
-print("Actual classes", labels)
+
+test_loss = 0
+test_acc = 0
+with torch.no_grad():
+    for x, y in test_loader:
+        x,y = x.to(device), y.to(device)
+        pred = model(x)
+ 
+        loss = criterion(pred, y)
+
+        test_loss += loss.item()
+
+        test_acc += accuracy(pred, y)
+
+    test_loss /= len(test_loader)
+    test_acc /= len(test_loader)
+print(f'Test Accuracy: {test_acc}, Test Loss: {test_loss}')    
+
+
+
+#TESTING 2
+model.eval()
+with torch.no_grad():
+    inputs, labels = next(iter(test_loader))
+    inputs, labels = inputs.to(device), labels.to(device)
+    outputs = model(inputs)
+    print(outputs)
+    print("Predicted classes", outputs.argmax(-1))
+    print("Actual classes", labels)
+
+print("DR classifier model completed")
